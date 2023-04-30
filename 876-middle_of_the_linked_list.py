8f505f3927353e8f179736ec2649f64999f55468
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-01 01:12:31 +0530

    Update 876-middle_of_the_linked_list.py

--- 876-middle_of_the_linked_list.py
+++ 876-middle_of_the_linked_list.py
@@ -5,10 +5,19 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        stack = []
-        h = head
-        while h != None:
-            stack.append(h)
-            h = h.next
+        # While slow moves one step forward, fast moves two steps forward.
+        # Finally, when fast reaches the end, slow happens to be in the middle of the linked list.
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+
+        # stack = []
+        # h = head
+        # while h != None:
+        #     stack.append(h)
+        #     h = h.next
         
-        return stack[ceil((len(stack)+1)/2)-1]
+        # return stack[ceil((len(stack)+1)/2)-1]
