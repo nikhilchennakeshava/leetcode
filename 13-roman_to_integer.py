7ed18af857a7b3e03c3f166b1279beea8c6efa87
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-04-30 14:03:42 +0530

    Create 13-roman_to_integer.py

---
+++ 13-roman_to_integer.py
@@ -0,0 +1,40 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        val = 0
+        prev = ''
+        for char in s:
+            match char:
+                case 'I':
+                    val += 1;
+                case 'V':
+                    if prev == 'I':
+                        val += 3
+                    else:
+                        val += 5
+                case 'X':
+                    if prev == 'I':
+                        val += 8
+                    else:
+                        val += 10
+                case 'L':
+                    if prev == 'X':
+                        val += 30
+                    else:
+                        val += 50
+                case 'C':
+                    if prev == 'X':
+                        val += 80
+                    else:
+                        val += 100
+                case 'D':
+                    if prev == 'C':
+                        val += 300
+                    else:
+                        val += 500
+                case 'M':
+                    if prev == 'C':
+                        val += 800
+                    else:
+                        val += 1000
+            prev = char
+        return val
