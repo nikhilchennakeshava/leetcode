ca83ed55d9eeef38a16232135dafada44e05e0c0
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-02 12:07:46 +0530

    Create 196-delete_duplicate_email.sql

---
+++ 196-delete_duplicate_email.sql
@@ -0,0 +1,6 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+
+DELETE P1 FROM Person P1, Person P2
+WHERE P1.Email = P2.Email AND
+P1.Id > P2.Id
