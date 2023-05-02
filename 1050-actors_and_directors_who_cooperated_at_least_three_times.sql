b5bcba880e02a23fbc77a19b72dda36fcac382e3
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-02 23:39:44 +0530

    Create 1050-actors_and_directors_who_cooperated_at_least_three_times.sql

---
+++ 1050-actors_and_directors_who_cooperated_at_least_three_times.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+select actor_id, director_id
+from ActorDirector group by actor_id, director_id having count(1) > 2;
+
+# select actor_id, director_id
+# from ActorDirector group by actor_id, director_id having count(timestamp) > 2;
