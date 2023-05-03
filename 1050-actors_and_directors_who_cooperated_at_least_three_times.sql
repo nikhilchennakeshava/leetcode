d750e0c610c4d148e3348f5700fcf07e70accb46
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-03 12:10:04 +0530

    Update 1050-actors_and_directors_who_cooperated_at_least_three_times.sql

--- 1050-actors_and_directors_who_cooperated_at_least_three_times.sql
+++ 1050-actors_and_directors_who_cooperated_at_least_three_times.sql
@@ -11,7 +11,9 @@ Table: ActorDirector
 timestamp is the primary key column for this table.
 
 Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+
 Return the result table in any order.
+
 The query result format is in the following example.
 
 Example 1:
