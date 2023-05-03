d201b00dd540e9ead8fc676ca4744ba3bc90ce94
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-03 12:39:36 +0530

    Update 184-department_highest_salary.sql

--- 184-department_highest_salary.sql
+++ 184-department_highest_salary.sql
@@ -1,8 +1,77 @@
+/*
+Table: Employee
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key column for this table.
+departmentId is a foreign key of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table. It is guaranteed that department name is not NULL.
+Each row of this table indicates the ID of a department and its name.
+
+Write an SQL query to find employees who have the highest salary in each of the departments.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Jim   | 90000  | 1            |
+| 3  | Henry | 80000  | 2            |
+| 4  | Sam   | 60000  | 2            |
+| 5  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output: 
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Jim      | 90000  |
+| Sales      | Henry    | 80000  |
+| IT         | Max      | 90000  |
++------------+----------+--------+
+Explanation: Max and Jim both have the highest salary in the IT department and Henry has the highest salary in the Sales department.
+*/
+
+
 # Write your MySQL query statement below
-with t as
-(select max(salary) Salary, Department.name Department, departmentId
-from Employee join Department on Employee.departmentId = Department.id 
-group by departmentId)
-select t.Department Department, Employee.name Employee, Employee.salary Salary
-from Employee join t on Employee.departmentId = t.departmentId
-where Employee.salary = t.Salary;
+
+select d.name Department, e.name Employee, e.salary Salary
+from Employee e join Department d on e.departmentId = d.id
+where e.salary = (select max(e2.salary) from Employee e2 where e.departmentId = e2.departmentId group by e2.departmentId);
+
+# with t as
+# (select max(salary) Salary, Department.name Department, departmentId
+# from Employee join Department on Employee.departmentId = Department.id 
+# group by departmentId)
+# select t.Department Department, Employee.name Employee, Employee.salary Salary
+# from Employee join t on Employee.departmentId = t.departmentId
+# where Employee.salary = t.Salary;
