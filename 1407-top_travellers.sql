11e3c290e508ce4c337bfa55e03ecb5d6b3475de
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-03 12:04:33 +0530

    Update 1407-top_travellers.sql

--- 1407-top_travellers.sql
+++ 1407-top_travellers.sql
@@ -75,6 +75,7 @@ Donald did not have any rides, the distance traveled by him is 0.
 
 
 # Write your MySQL query statement below
+
 select name, if(sum(distance) is not null, sum(distance), 0) travelled_distance
 from Users left join Rides on Users.id = Rides.user_id
 group by Users.id order by travelled_distance desc, name;
