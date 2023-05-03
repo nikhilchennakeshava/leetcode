62ac1f406a071c638046e41555a276f012a16634
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-03 12:08:48 +0530

    Update 1407-top_travellers.sql

--- 1407-top_travellers.sql
+++ 1407-top_travellers.sql
@@ -23,7 +23,9 @@ id is the primary key for this table.
 user_id is the id of the user who traveled the distance "distance".
 
 Write an SQL query to report the distance traveled by each user.
+
 Return the result table ordered by travelled_distance in descending order, if two or more users traveled the same distance, order them by their name in ascending order.
+
 The query result format is in the following example.
 
 Example 1:
