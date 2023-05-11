8f28a29acd4ddf4ae4b6f3b66ddeea59d8ca7c4b
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-11 05:32:27 +0530

    Create 387-first_unique_character_in_a_string.py

---
+++ 387-first_unique_character_in_a_string.py
@@ -0,0 +1,43 @@
+'''
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+Constraints:
+
+    1 <= s.length <= 105
+    s consists of only lowercase English letters.
+'''
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count_dict = {}
+        for ch in s:
+            count_dict[ch] = count_dict.get(ch, 0) + 1
+
+        for i in range(len(s)):
+            if count_dict[s[i]] == 1: return i
+        return -1
+
+        # count_dict = {}
+        # for ch in s:
+        #     count_dict[ch] = count_dict.get(ch, 0) + 1
+
+        # for ch in count_dict:
+        #     if count_dict[ch] == 1: return s.find(ch)
+        # else:
+        #     return -1
