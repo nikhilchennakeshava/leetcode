80dfaa0ff7a92d7ab0025746a5c088c22e8d826e
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-13 00:50:49 +0530

    Create 1991-find_the_middle_index_in_array.py

---
+++ 1991-find_the_middle_index_in_array.py
@@ -0,0 +1,54 @@
+'''
+Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+
+A middleIndex is an index where nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1].
+
+If middleIndex == 0, the left side sum is considered to be 0. Similarly, if middleIndex == nums.length - 1, the right side sum is considered to be 0.
+
+Return the leftmost middleIndex that satisfies the condition, or -1 if there is no such index.
+
+Example 1:
+
+Input: nums = [2,3,-1,8,4]
+Output: 3
+Explanation: The sum of the numbers before index 3 is: 2 + 3 + -1 = 4
+The sum of the numbers after index 3 is: 4 = 4
+
+Example 2:
+
+Input: nums = [1,-1,4]
+Output: 2
+Explanation: The sum of the numbers before index 2 is: 1 + -1 = 0
+The sum of the numbers after index 2 is: 0
+
+Example 3:
+
+Input: nums = [2,5]
+Output: -1
+Explanation: There is no valid middleIndex.
+
+Constraints:
+
+    1 <= nums.length <= 100
+    -1000 <= nums[i] <= 1000
+
+Note: This question is the same as 724: https://leetcode.com/problems/find-pivot-index/
+'''
+
+
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        left = 0 # nums[0] + nums[1] + ... + nums[middleIndex-1]
+        right = sum(nums) # nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1]
+
+        for i, num in enumerate(nums): # we can use normal for loop as well.
+            right -= num # as we are trying to find out middle index so iteratively we`ll reduce the value of right to find the middle index
+            if left == right: # comparing the values for finding out the middle index.
+                return i # if there is any return the index whixh will be our required index.
+            left += num # we have to add the num iteratively. 
+        return -1
+
+        # for i in range(len(nums)):
+        #     if sum(nums[:i]) == sum(nums[i+1:]):
+        #         return i
+        # return -1
