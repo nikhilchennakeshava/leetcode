52b41349f8dc22e65eb7711d9ec2ff7e8c940480
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-13 12:08:08 +0530

    Update 171-excel_sheet_column_number.py

--- 171-excel_sheet_column_number.py
+++ 171-excel_sheet_column_number.py
@@ -1,4 +1,6 @@
 '''
+171. Excel Sheet Column Number
+
 Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
 
 For example:
