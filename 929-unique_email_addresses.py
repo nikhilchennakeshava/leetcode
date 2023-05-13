7a89588df99882832a558003b25bfa02b727dab2
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-13 11:56:04 +0530

    Update 929-unique_email_addresses.py

--- 929-unique_email_addresses.py
+++ 929-unique_email_addresses.py
@@ -1,4 +1,6 @@
 '''
+929. Unique Email Addresses
+
 Every valid email consists of a local name and a domain name, separated by the '@' sign. Besides lowercase letters, the email may contain one or more '.' or '+'.
 
     For example, in "alice@leetcode.com", "alice" is the local name, and "leetcode.com" is the domain name.
