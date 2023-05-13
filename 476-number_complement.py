24fe1ebf82e0be46e8bcb9f91b35740fac790f9f
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-13 12:02:14 +0530

    Update 476-number_complement.py

--- 476-number_complement.py
+++ 476-number_complement.py
@@ -1,9 +1,5 @@
 '''
 476. Number Complement
-Easy
-2.4K
-117
-Companies
 
 The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
 
