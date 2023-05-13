8f5a345f44ad0ca8705183b542226cafa8409666
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-13 11:44:35 +0530

    Update 2469-convert_the_temperature.py

--- 2469-convert_the_temperature.py
+++ 2469-convert_the_temperature.py
@@ -1,4 +1,6 @@
 '''
+2469. Convert the Temperature
+
 You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
 
 You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
