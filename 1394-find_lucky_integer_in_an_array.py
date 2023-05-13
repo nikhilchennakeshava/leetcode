87af20d1bf7be3e567160439c6d71f7c16476105
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-13 11:49:17 +0530

    Update 1394-find_lucky_integer_in_an_array.py

--- 1394-find_lucky_integer_in_an_array.py
+++ 1394-find_lucky_integer_in_an_array.py
@@ -1,4 +1,6 @@
 '''
+1394. Find Lucky Integer in an Array
+
 Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
 
 Return the largest lucky integer in the array. If there is no lucky integer return -1.
