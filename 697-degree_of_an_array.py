f77172c9a3bdbf341ebf8f187bb21efb8e5b092f
Nikhil Chennakeshava <19240412+nikhilchennakeshava@users.noreply.github.com>
2023-05-16 12:47:58 +0530

    Create 697-degree_of_an_array.py

---
+++ 697-degree_of_an_array.py
@@ -0,0 +1,49 @@
+'''
+697. Degree of an Array
+
+Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
+
+Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
+
+Example 1:
+
+Input: nums = [1,2,2,3,1]
+Output: 2
+Explanation: 
+The input array has a degree of 2 because both elements 1 and 2 appear twice.
+Of the subarrays that have the same degree:
+[1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+The shortest length is 2. So return 2.
+
+Example 2:
+
+Input: nums = [1,2,2,3,1,4,2]
+Output: 6
+Explanation: 
+The degree is 3 because the element 2 is repeated 3 times.
+So [2,2,3,1,4,2] is the shortest subarray, therefore returning 6.
+
+Constraints:
+
+    nums.length will be between 1 and 50,000.
+    nums[i] will be an integer between 0 and 49,999.
+'''
+
+
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        nums_dict,nums_len,nums_rev = Counter(nums),len(nums),nums[::-1]
+        max_elements = [key for key,value in nums_dict.items() if value == max(nums_dict.values())]
+
+        return min(nums_len - nums_rev.index(i) - nums.index(i) for i in max_elements)
+
+        # first, count, res, degree = {}, {}, 0, 0
+        # for i, a in enumerate(A):
+        #     first.setdefault(a, i)
+        #     count[a] = count.get(a, 0) + 1
+        #     if count[a] > degree:
+        #         degree = count[a]
+        #         res = i - first[a] + 1
+        #     elif count[a] == degree:
+        #         res = min(res, i - first[a] + 1)
+        # return res
